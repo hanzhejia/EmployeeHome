8fed4146b9864fbc072cb2610b1235d1ef81718d
causehhc <1005694755@qq.com>
2021-06-18 14:16:10 +0800

    updated download

---
+++ frontend/mock/download.js
@@ -0,0 +1,93 @@
+const Mock = require('mockjs')
+
+const List = []
+const count = 100
+
+for (let i = 0; i < count; i++) {
+  List.push(Mock.mock({
+    id: '@increment',
+    title: '@title(1, 5)',
+    time: +Mock.Random.date('T'),
+    master: '@first',
+    desc: '@title(5, 10)'
+  }))
+}
+
+module.exports = [
+  {
+    url: '/vue-admin-template/download/list',
+    type: 'get',
+    response: config => {
+      const { page = 1, limit = 20 } = config.query
+
+      const mockList = List
+
+      const pageList = mockList.filter((item, index) => index < limit * page && index >= limit * (page - 1))
+
+      return {
+        code: 20000,
+        data: {
+          total: mockList.length,
+          items: pageList
+        }
+      }
+    }
+  },
+
+  {
+    url: '/vue-admin-template/download/detail',
+    type: 'get',
+    response: config => {
+      const { id } = config.query
+      for (const article of List) {
+        if (article.id === +id) {
+          return {
+            code: 20000,
+            data: article
+          }
+        }
+      }
+    }
+  },
+
+  {
+    url: '/vue-admin-template/download/pv',
+    type: 'get',
+    response: _ => {
+      return {
+        code: 20000,
+        data: {
+          pvData: [
+            { key: 'PC', pv: 1024 },
+            { key: 'mobile', pv: 1024 },
+            { key: 'ios', pv: 1024 },
+            { key: 'android', pv: 1024 }
+          ]
+        }
+      }
+    }
+  },
+
+  {
+    url: '/vue-admin-template/download/create',
+    type: 'post',
+    response: _ => {
+      return {
+        code: 20000,
+        data: 'success'
+      }
+    }
+  },
+
+  {
+    url: '/vue-admin-template/download/update',
+    type: 'post',
+    response: _ => {
+      return {
+        code: 20000,
+        data: 'success'
+      }
+    }
+  }
+]
+
