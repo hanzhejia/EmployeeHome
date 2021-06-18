fd9070b5d349719b5fb1d4997e298895a8a31ac5
Bing-jangnan <1004517408@qq.com>
2021-06-18 14:36:19 +0800

    lql em

--- frontend/src/views/employee_manage/func1/index.vue
+++ frontend/src/views/employee_manage/func1/index.vue
@@ -1,18 +1,138 @@
 <template>
   <div class="func1-container">
-    <div class="func1-text">3-1name: {{ name }}</div>
+    <switch-roles @change="handleRolesChange" />
+    <el-table
+      :data="tableData.filter(data => !search || data.name.includes(search))"
+      style="width: 100%"
+    >
+      <el-table-column
+        type="selection"
+        width="55"
+      />
+      <el-table-column
+        label="姓名"
+        prop="name"
+      />
+      <el-table-column
+        label="性别"
+        prop="sex"
+      />
+      <el-table-column
+        label="手机号"
+        prop="phone"
+      />
+      <el-table-column
+        label="邮箱"
+        prop="email"
+      />
+      <el-table-column
+        label="职位"
+        prop="job"
+      />
+      <el-table-column
+        label="学历"
+        prop="education"
+      />
+      <el-table-column
+        label="身份证号码"
+        prop="card"
+      />
+      <el-table-column
+        label="部门"
+        prop="dept"
+      />
+      <el-table-column
+        label="联系地址"
+        prop="address"
+      />
+      <el-table-column
+        label="建档日期"
+        prop="createdate"
+      />
+      <el-table-column
+        label="操作"
+        prop="operation"
+      >
+        <template>
+          <el-button v-if="checkPermission(['admin'])" size="mini" type="danger" @click="operation()">编辑</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column align="right">
+        <template slot="header">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入姓名搜索"
+          />
+        </template>
+      </el-table-column>
+    </el-table>
+
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+// import { mapGetters } from 'vuex'
+import checkPermission from '@/utils/permission'
 
 export default {
   name: 'Func1',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  // computed: {
+  //   ...mapGetters([
+  //     'name'
+  //   ])
+  // },
+  data() {
+    return {
+      tableData: [
+        {
+          name: '甄珍器',
+          sex: '男',
+          phone: '12345678900',
+          email: '1004517408@qq.com',
+          job: '蒙古海军司令',
+          education: '大专人',
+          card: '123456789456123046',
+          dept: '宣传部',
+          address: '翻斗花园',
+          createdate: '111122222'
+        }
+      ],
+      search: ''
+    }
+  },
+  methods: {
+    checkPermission,
+    handleRolesChange() {
+      this.key++
+    },
+    handleDownload(index, row) {
+      console.log(index, row)
+    },
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    },
+    operation() {
+      this.$prompt('请输入邮箱', '编辑', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+        inputErrorMessage: '邮箱格式不正确'
+      }).then(({ value }) => {
+        this.$message({
+          type: 'success',
+          message: '你的邮箱是: ' + value
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        })
+      })
+    }
   }
 }
 </script>
@@ -22,9 +142,9 @@ export default {
   &-container {
     margin: 30px;
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
+  //&-text {
+  //  font-size: 30px;
+  //  line-height: 46px;
+  //}
 }
 </style>
--- frontend/src/views/employee_manage/func2/index.vue
+++ frontend/src/views/employee_manage/func2/index.vue
@@ -1,18 +1,198 @@
 <template>
-  <div class="func2-container">
-    <div class="func2-text">3-2name: {{ name }}</div>
+  <div>
+    <el-form :ref="ruleForm" :model="ruleForm" label-width="100px" class="demo-ruleForm">
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="ruleForm.name" placeholder="姓名"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="身份证号" prop="card">
+            <el-input v-model="ruleForm.card"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="性别" prop="sex">
+            <el-select v-model="ruleForm.sex" placeholder="♂/♀">
+              <el-option label="男" value="男"></el-option>
+              <el-option label="女" value="女"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="职位" prop="job">
+            <el-select v-model="ruleForm.job" >
+              <el-option label="蒙古海军司令" value="蒙古海军司令"></el-option>
+              <el-option label="梵蒂冈空军最高指挥官" value="梵蒂冈空军最高指挥官"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="学历" prop="education">
+            <el-input v-model="ruleForm.education"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="ruleForm.email"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="电话" prop="tel">
+            <el-input v-model="ruleForm.tel"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="手机" prop="phone">
+            <el-input v-model="ruleForm.phone"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="政治面貌" prop="party">
+            <el-input v-model="ruleForm.party"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="QQ号码" prop="qqnum">
+            <el-input v-model="ruleForm.qqnum"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="联系地址" prop="address">
+            <el-input v-model="ruleForm.address" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="邮政编码" prop="post">
+            <el-input v-model="ruleForm.post" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="出生日期" required>
+            <el-col :span="11">
+              <el-form-item prop="date">
+                <el-date-picker
+                  v-model="ruleForm.birthday"
+                  type="date"
+                  placeholder="选择日期"
+                  style="width: 100%;"
+                ></el-date-picker>
+              </el-form-item>
+            </el-col>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="民族" prop="race">
+            <el-input v-model="ruleForm.race"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="所学专业" prop="speciality">
+            <el-input v-model="ruleForm.speciality"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="爱好" prop="hobby">
+            <el-input v-model="ruleForm.hobby"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="10">
+          <el-form-item label="备注" prop="remark">
+            <el-input v-model="ruleForm.remark"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10" :offset="2">
+          <el-form-item label="所属部门" prop="dept">
+            <el-select v-model="ruleForm.dept" >
+              <el-option label="人事部" value="人事部"></el-option>
+              <el-option label="采购部" value="采购部"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">添加</el-button>
+        <el-button @click="resetForm('ruleForm')">取消</el-button>
+      </el-form-item>
+    </el-form>
+
   </div>
 </template>
-
 <script>
-import { mapGetters } from 'vuex'
+// import { mapGetters } from 'vuex'
 
 export default {
   name: 'Func2',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  // computed: {
+  //   ...mapGetters([
+  //     'name'
+  //   ])
+  // }
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        card: '',
+        sex: '',
+        job: '',
+        education: '',
+        email: '',
+        tel: '',
+        phone: '',
+        party: '',
+        qqnum: '',
+        address: '',
+        post: '',
+        birthday: '',
+        race: '',
+        speciality: '',
+        hobby: '',
+        remark: '',
+        dept: ''
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
   }
 }
 </script>
@@ -22,9 +202,10 @@ export default {
   &-container {
     margin: 30px;
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
+
+  //&-text {
+  //  font-size: 30px;
+  //  line-height: 46px;
+  //}
 }
 </style>
