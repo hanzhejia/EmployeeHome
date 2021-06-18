f6694cff151b7dd59fc852e856c195affc745642
ljlat <2315816910@qq.com>
2021-06-18 15:11:50 +0800

    update lj

--- frontend/src/views/login/func2/index.vue
+++ frontend/src/views/login/func2/index.vue
@@ -1,106 +1,119 @@
 <template>
-  <div class="func1-container">
-    <div class="func1-text">x-1name: {{ name }}</div>
-    <div class="func1-list">
-      <li v-for="(item, index) in list" :key="index" class="list-item">
-        <el-card class="box-card" shadow="hover">
-          <div class="style1">{{ item.info1 }}</div>
-          <div class="style2">{{ item.info2 }}</div>
-          <el-button style="float: right; padding: 3px 0" type="text" @click="func_post_something(item)">
-            func_post_something
-          </el-button>
-          <el-button style="float: right; padding: 3px 0" type="text" @click="func_get_something(item)">
-            func_get_something
-          </el-button>
-          <el-button style="float: right; padding: 3px 0" type="text" @click="func_do_something()">
-            func_do_something
-          </el-button>
-        </el-card>
-      </li>
-    </div>
+  <div class="app-container common-list-page">
+    <el-form
+      ref="resetForm"
+      :model="resetForm"
+      :rules="resetFormRules"
+      status-icon
+      label-width="100px"
+    >
+      <el-form-item label="旧密码：" prop="password">
+        <el-input v-model="resetForm.password" type="password" show-password auto-complete="off" />
+      </el-form-item>
+      <el-form-item label="新密码：" prop="newpwd">
+        <el-input v-model="resetForm.newpwd" type="password" show-password auto-complete="off" />
+      </el-form-item>
+      <el-form-item label="确认密码：" prop="newpassword1">
+        <el-input v-model="resetForm.newpassword1" type="password" show-password auto-complete="off" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click.native.prevent="toAmend">确认修改</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { do_something, get_something, post_something } from '@/api/test'
-
 export default {
-  name: 'Func1',
   data() {
+    var validatePass = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('请输入新密码'))
+      } else if (value.toString().length < 6 || value.toString().length > 18) {
+        callback(new Error('密码长度为6-18位'))
+      } else {
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.resetForm.newpwd) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
-      count: 0,
-      list: [
-        { info1: '122223', info2: '456' },
-        { info1: '789', info2: '111' }
-      ]
+      resetForm: {
+        // 传给后台所需要的参数
+        newpassword1: '',
+        password: '',
+        username: ''// 此处只是后台需要的字段而已，如果前期有公用cookie里面有获取并且保存过，现在需要另外调用进来，具体的获取方法就看个人了
+      },
+      resetFormRules: {
+        password: [
+          { required: true, message: '请输入旧密码', trigger: 'blur' }
+        ],
+        newpwd: [
+          { required: true, validator: validatePass, trigger: 'blur' }
+        ],
+        newpassword1: [
+          { required: true, validator: validatePass2, trigger: 'blur' }
+        ]
+      }
     }
   },
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
-  },
   methods: {
-    func_post_something(data) {
-      this.list = [
-        { info1: '1223', info2: '456' },
-        { info1: '789', info2: '111' }
-      ]
-      // setTimeout(() => {
-      //   post_something({
-      //     count: this.count
-      //   }).then(response => {
-      //     console.log('func_post_sth')
-      //   })
-      // }, 10)
+    toAmend() {
+      this.$refs.resetForm.validate(valid => {
+        if (valid) {
+          this.open()
+          // window.alert('ok')
+          // 这里的api.materialQuery.toAmend是调用前期我们统一的api接口url路径，不作参考 ，只要把后台需要的字段正常传进去即可
+          // eslint-disable-next-line no-undef
+          // api.materialQuery.toAmend(this.resetForm)
+          //   .then(res => {
+          //     if (res.code === 2) {
+          //       this.$message({
+          //         message: res.msg,
+          //         type: 'error',
+          //         duration: '2000'
+          //       })
+          //       return false
+          //     }
+          //     if (res.code === 0) {
+          //       this.$message.success('修改成功,3秒后跳转到登录页！')
+          //       setTimeout(() => {
+          //         this.logout()// 调用跳转到登陆页的方法
+          //       }, 3000)
+          //     }
+          //     // eslint-disable-next-line no-undef
+          //     ic
+          //   })
+          //   .catch(() => {})
+        }
+      })
     },
-    func_get_something(token) {
-      setTimeout(() => {
-        get_something({
-          count: this.count
-        }).then(response => {
-          console.log('func_get_sth')
-        })
-      }, 10)
+    // 这是修改成功后重新返回登陆页的方法，看个人需要自行调整
+    async logout() {
+      await this.$store.dispatch('user/logout')
+      this.$router.push(`/login`)
     },
-    func_do_something() {
-      setTimeout(() => {
-        do_something({
-          count: this.count
-        }).then(response => {
-          console.log('func_do_sth')
-        })
-      }, 10)
+    open() {
+      this.$alert('修改成功', '提示')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.func1 {
-  &-container {
-    margin: 30px;
+.el-form {
+  width: 60%;
+  margin: 50px auto 0;
+  text-align: center;
+  button {
+    margin: 20px 0 0;
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
-
-.func1-list {
-  margin: 0 auto;
-  padding-right: 5px;
-  font-size: 14px;
-}
-.list-item {
-  list-style: none;
-  display:flex;
-  justify-content: center;
-  margin-bottom: 18px;
-}
-.box-card {
-  width: 100%;
-  border-radius: 20px
 }
 </style>
