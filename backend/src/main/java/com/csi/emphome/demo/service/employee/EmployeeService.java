b30cfabea61bcc1fcfdc3fd6c2f1704235870cc9
Bing-jangnan <1004517408@qq.com>
2021-06-21 09:14:40 +0800

    lql/9

--- backend/src/main/java/com/csi/emphome/demo/service/employee/EmployeeService.java
+++ backend/src/main/java/com/csi/emphome/demo/service/employee/EmployeeService.java
@@ -1,9 +1,10 @@
 package com.csi.emphome.demo.service.employee;
-
+import com.csi.emphome.demo.domain.employee.EmployeeItem;
 import org.springframework.stereotype.Service;
 import java.util.HashMap;
 
 @Service
 public interface EmployeeService {
     HashMap<String, Object> fetchListFunc();
+    HashMap<String, Object> createListItemFunc(EmployeeItem data);
 }
--- backend/src/main/java/com/csi/emphome/demo/service/employee/impl/EmployeeServiceImpl.java
+++ backend/src/main/java/com/csi/emphome/demo/service/employee/impl/EmployeeServiceImpl.java
@@ -1,4 +1,5 @@
 package com.csi.emphome.demo.service.employee.impl;
+import com.csi.emphome.demo.domain.employee.EmployeeItem;
 import com.csi.emphome.demo.repository.employee.EmployeeRepository;
 import com.csi.emphome.demo.service.employee.EmployeeService;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         responseData.put("items",listItems);
         response.put("code",20000);
         response.put("data",responseData);
+        return response;
+    }
+
+    @Override
+    public HashMap<String, Object> createListItemFunc(EmployeeItem data) {
+//        testRepository.save(data);
+        System.out.println(data);
+
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("code",20000);
+        response.put("data","success");
         return response;
     }
 }
