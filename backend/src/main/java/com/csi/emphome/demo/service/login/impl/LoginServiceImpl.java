b5ac70ea9f4f6cb85370fe63fc391e8bb73394d1
ljlat <2315816910@qq.com>
2021-06-22 09:02:20 +0800

    update lj

--- backend/src/main/java/com/csi/emphome/demo/service/login/impl/LoginServiceImpl.java
+++ backend/src/main/java/com/csi/emphome/demo/service/login/impl/LoginServiceImpl.java
@@ -1,22 +1,46 @@
 package com.csi.emphome.demo.service.login.impl;
 
 import com.csi.emphome.demo.domain.test.TestItem;
+import com.csi.emphome.demo.repository.login.LoginRepo;
 import com.csi.emphome.demo.repository.test.TestRepository;
 import com.csi.emphome.demo.service.login.LoginService;
+import com.csi.emphome.demo.service.login.dto.LoginTemp;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public class LoginServiceImpl implements LoginService {
-    private final TestRepository testRepository;
-
-    public LoginServiceImpl(TestRepository testRepository) {
-        this.testRepository = testRepository;
-    }
-
+  //  private final TestRepository testRepository;
+    private final LoginRepo loginRepo;
+   // public LoginServiceImpl(TestRepository testRepository) {
+       // this.testRepository = testRepository;
+  //  }
+    public LoginServiceImpl(LoginRepo loginRepo){this.loginRepo=loginRepo;}
     @Override
-    public List<TestItem> list() {
-        return testRepository.findAll();
+    public HashMap<String, Object> createLoginItemFunc(LoginTemp data) {
+        System.out.println("createLoginItemFunc:"+data);
+        HashMap<String, Object> response=new HashMap<>();
+        HashMap<String, Object> responseData = new HashMap<>();
+        if (data.getUsername().equals("admin")&&data.getPassword().equals("111111")){
+            response.put("code",20000);
+            response.put("data",responseData);
+            responseData.put("token",1);
+            response.put("msg","登录成功");
+        }
+        else {
+            response.put("code",50000);
+            response.put("data",responseData);
+            responseData.put("token",0);
+            response.put("msg","登录失败");
+        }
+
+        return response;
     }
+
+    // @Override
+   // public List<TestItem> list() {
+      //  return testRepository.findAll();
+  //  }
 }
