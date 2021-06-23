05c8e4fac292eeccb5903b9cc3be4bedab96ef4a
sy777 <55306927+sy777@users.noreply.github.com>
2021-06-23 14:24:42 +0800

    updated xxx

--- backend/src/main/java/com/csi/emphome/demo/service/face/impl/FaceServiceImpl.java
+++ backend/src/main/java/com/csi/emphome/demo/service/face/impl/FaceServiceImpl.java
@@ -1,28 +1,25 @@
 package com.csi.emphome.demo.service.face.impl;
-
 import com.csi.emphome.demo.domain.face.Face;
 import com.csi.emphome.demo.repository.face.FaceRepository;
 import com.csi.emphome.demo.service.face.FaceService;
+import com.csi.emphome.demo.repository.user.UserRepository;
+import com.csi.emphome.demo.service.user.dto.UserListQuery;
 import org.json.JSONException;
 import org.springframework.stereotype.Service;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import com.baidu.aip.face.AipFace;
 import com.baidu.aip.face.MatchRequest;
 import com.csi.emphome.demo.service.face.util.BaiduAIPCommon;
-import com.csi.emphome.demo.service.face.util.Base64Util;
-import com.csi.emphome.demo.service.face.util.FileUtil;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 @Service
 public class FaceServiceImpl implements FaceService {
     private final FaceRepository faceRepository;
-
-    public FaceServiceImpl(FaceRepository faceRepository) {
+    private final UserRepository userRepository;
+    public FaceServiceImpl(FaceRepository faceRepository, UserRepository userRepository) {
         this.faceRepository = faceRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -51,22 +48,7 @@ public class FaceServiceImpl implements FaceService {
         HashMap<String, Object> responseData = new HashMap<>();
         List<Face> listItems = faceRepository.findAll();
         AipFace client = new AipFace(BaiduAIPCommon.APP_FACE_ID, BaiduAIPCommon.API_FACE_KEY, BaiduAIPCommon.SECRET_FACE_KEY);
-//        Map entry = (Map) listItems.get(0);
-//        String userface = entry.get("base64").toString();
-//        System.out.println(userface);
-//        String imgStr2 = listItems.get(0).getBase64();
-//        String imgStr = nowbase64.substring(0, nowbase64.length() - 1);
         String imgStr = nowbase64.getBase64();
-//        System.out.print(imgStr);
-//        System.out.print("\nimgStr");
-
-//        MatchRequest req1 = new MatchRequest(imgStr, "BASE64");
-//        MatchRequest req2 = new MatchRequest(imgStr2, "BASE64");
-//        ArrayList<MatchRequest> reqs = new ArrayList<>();
-//        reqs.add(req1);
-//        reqs.add(req2);
-//        JSONObject res = client.match(reqs);
-//        System.out.println(res);
         int i = 0;
         String imgStr2;
         for (i=0;i<listItems.size();i++){
